c8394d7117e8cfbc881da850467e0d090ce574c3
Danny Knapp <dknapp@salesforce.com>
2014-04-04 17:30:51 -0700

    Fixing issues with installing plugins and boxes as current user

--- resources/box.rb
+++ resources/box.rb
@@ -5,3 +5,4 @@ default_action :install
 attribute :box_name, name_attribute: true
 attribute :uri, kind_of: [String]
 attribute :installed, kind_of: [TrueClass, FalseClass]
+attribute :user, kind_of: [String]
